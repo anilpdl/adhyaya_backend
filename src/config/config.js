4e942e637b7d96cf9f6655b332b4fd3c47232cc3
anlpdl <anil@truemark.com.np>
2019-08-10 21:20:36 +0545

    initial commit

---
+++ src/config/config.js
@@ -0,0 +1,17 @@
+const {
+  DB_NAME, DB_PASSWORD, DB_USER, DB_HOST, DB_CLIENT
+} = process.env;
+
+const config = {
+  db: {
+    client: DB_CLIENT,
+    connection: {
+      host: DB_HOST,
+      user: DB_USER,
+      password: DB_PASSWORD,
+      database: DB_NAME
+    }
+  }
+};
+
+export default config;
