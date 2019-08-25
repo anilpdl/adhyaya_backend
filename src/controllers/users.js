302a2bd187ca1b4c84e33e2f6684e553cfa51d42
anlpdl <anil@truemark.com.np>
2019-08-25 22:31:35 +0545

    user verification added

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -4,8 +4,15 @@ import User from '../models/User';
 
 import config from '../config/jsonconfig';
 
+const responseMessage = {
+  SIGNIN_SUCCESS: 'Signed in successfully',
+  SIGNIN_EMPTY: 'Invalid Credentials Provided',
+  SIGNIN_ERROR: 'Incorrect Email or Password '
+}
+
 export const sign_up = async (req, res) => {
   const { email, last_name, first_name, password, gender } = req.body;
+  console.log(req.body)
   if(!email || !password || !first_name || !last_name) {
     res.status(400).send({
       message: 'Missing some informations'
@@ -19,6 +26,7 @@ export const sign_up = async (req, res) => {
   }
   bcrypt.hash(password, 10, (err, hash) => {
     if (err) {
+      console.log(err)
       return res.status(500).send({
         message: err
       });
@@ -53,9 +61,9 @@ export const sign_up = async (req, res) => {
 export const sign_in = function (req, res) {
   const { email, password } = req.body;
   if(!email || !password) {
-    res.status(400).send({
-      message: 'Invalid Credentials Provided'
-    });
+    res.status(400).send(JSON.stringify({
+      message: responseMessage.SIGNIN_EMPTY
+    }));
 
     return;
   }
@@ -64,7 +72,7 @@ export const sign_in = function (req, res) {
       bcrypt.compare(password, user.get('password'), (err, result) => {
         if (err) {
           return res.status(401).send({
-            message: 'Incorrect Email or Password'
+            message: responseMessage.SIGNIN_ERROR
           });
         }
         if (result) {
@@ -75,26 +83,51 @@ export const sign_in = function (req, res) {
             },
             config.secret,
             {
-              expiresIn: '2h'
+              expiresIn: 60
             }
           );
           const bearerToken = `${'Bearer' + ' '}${JWTToken}`;
           return res.status(200).json({
             auth: true,
-            message: 'Signed in successfully!',
+            message: responseMessage.SIGNIN_SUCCESS,
             token: bearerToken,
             user
           });
         }
         return res.status(401).send({
-          message: 'Incorrect Email or Password'
+          message: responseMessage.SIGNIN_ERROR
         });
       });
     })
     .catch((error) => {
+      console.log(error)
       res.status(500).send({
         error,
-        message: 'Internal server error'
+        message: responseMessage.SIGNIN_ERROR
       });
     });
 };
+
+export const getDetail = async (req, res) => {
+  const { userId } = req.params;
+  const authToken = req.headers.authorization;
+  const token = authToken.replace('Bearer ','');
+  console.log(token)
+  jwt.verify(token, config.secret, async function(err, decoded) {
+    if (err) return res.status(401).send({ auth: false, message: 'Failed to authenticate token.' });
+    const { id } = decoded;
+    const user = await User.forge({ id }).fetch();
+    const role = user.get('role');
+    if(role == 'student') {
+      if(id != userId) {
+        return res.status(403).send({ message: 'Access Forbidden'})
+      }
+    }
+    if(userId) {
+      const user = await User.forge({ id: userId }).fetch();
+      return res.status(200).json({
+        user
+      });
+    }
+  });
+}
