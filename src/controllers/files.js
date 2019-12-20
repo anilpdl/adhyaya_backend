99d365175ffdd4f52160524c8bfb521649c453a6
anlpdl <anil@truemark.com.np>
2019-12-21 00:10:54 +0545

    fix user unable to delete files

--- src/controllers/files.js
+++ src/controllers/files.js
@@ -11,7 +11,7 @@ const responseMessage = {
   SUCCESS: 'Signed in successfully',
   ERROR: 'Something went wrong when uploading files',
   DELETE_ERROR: 'Error deleting file'
-}
+};
 
 export const getAll = async (req, res) => {
   const authData = await AuthController.checkAccess(req, res);
@@ -19,7 +19,7 @@ export const getAll = async (req, res) => {
     const files = await Files.getAll();
     res.status(200).send(files);
   }
-}
+};
 
 export const getUserFiles = async (req, res) => {
   const authData = await AuthController.checkAccess(req, res);
@@ -27,9 +27,9 @@ export const getUserFiles = async (req, res) => {
     const files = await Files.getUserFiles();
     res.status(200).send({ files });
   }
-}
+};
 
-const uploadFileToCloudinary = async (file) => {
+const uploadFileToCloudinary = async file => {
   try {
     const { originalname } = file;
     const fileToUpload = dataUri(file);
@@ -39,43 +39,53 @@ const uploadFileToCloudinary = async (file) => {
   } catch (err) {
     throw err;
   }
-}
+};
 
-const saveFiles = async (files) => {
+const saveFiles = async files => {
   try {
     const newFiles = await Files.bulkSave(files);
     return newFiles;
   } catch (err) {
     return err;
   }
-}
+};
 
 export const create = async (req, res) => {
   const { userId } = req.params;
   const { files } = req;
   if (files.length) {
     try {
-      const uploadedFiles = await Promise.all(files.map((file) => uploadFileToCloudinary(file)));
-      const uploadedFileUrls = uploadedFiles.map(({ format, secure_url, originalname }) => ({ name: originalname, url: secure_url, format }));
+      const uploadedFiles = await Promise.all(
+        files.map(file => uploadFileToCloudinary(file))
+      );
+      const uploadedFileUrls = uploadedFiles.map(
+        ({ format, secure_url, originalname }) => ({
+          name: originalname,
+          url: secure_url,
+          format
+        })
+      );
       const savedFiles = await saveFiles(uploadedFileUrls);
-      const userFiles = savedFiles.map(file => ({ file_id: file.id, user_id: parseInt(userId) }));
+      const userFiles = savedFiles.map(file => ({
+        file_id: file.id,
+        user_id: parseInt(userId)
+      }));
       const savedUserFiles = await UserFile.bulkSaveFile(userFiles);
       return res.status(200).json({
         files: savedUserFiles
       });
     } catch (err) {
-      console.log(err)
+      console.log(err);
       return res.status(400).json({
         message: responseMessage.ERROR
       });
     }
-  }
-  else {
+  } else {
     res.status(400).json({
       message: responseMessage.ERROR
-    })
+    });
   }
-}
+};
 
 export const createOrUpdateAvatar = async ({ url, user_id }) => {
   const avatar = await UserAvatar.getDetail(user_id);
@@ -86,7 +96,7 @@ export const createOrUpdateAvatar = async ({ url, user_id }) => {
 
   const userAvatar = await UserAvatar.create({ url, user_id });
   return userAvatar;
-}
+};
 
 export const createAvatar = async (req, res) => {
   const { userId } = req.params;
@@ -97,22 +107,19 @@ export const createAvatar = async (req, res) => {
       const uploadedFile = await uploadFileToCloudinary(file);
       const { secure_url: url } = uploadedFile;
       const userAvatar = await createOrUpdateAvatar({ url, user_id: userId });
-      return res.status(200).json(
-        userAvatar
-      );
+      return res.status(200).json(userAvatar);
     } catch (err) {
-      console.log(err)
+      console.log(err);
       return res.status(400).json({
         message: responseMessage.ERROR
       });
     }
-  }
-  else {
+  } else {
     res.status(400).json({
       message: responseMessage.ERROR
-    })
+    });
   }
-}
+};
 
 export const fetchDetail = async (req, res) => {
   const { fileId } = req.params;
@@ -121,15 +128,14 @@ export const fetchDetail = async (req, res) => {
     try {
       const file = await Files.getDetail(fileId);
       return res.send(file);
-    }
-    catch (err) {
-      console.log(err)
+    } catch (err) {
+      console.log(err);
       return res.status(400).json({
         message: responseMessage.ERROR
       });
     }
   }
-}
+};
 
 export const updateDetails = async (req, res) => {
   const { fileId } = req.params;
@@ -139,32 +145,27 @@ export const updateDetails = async (req, res) => {
     try {
       const file = await Files.updateDetails(fileId, approverId);
       return res.send(file);
-    }
-    catch (err) {
-      console.log(err)
+    } catch (err) {
+      console.log(err);
       return res.status(400).json({
         message: responseMessage.ERROR
       });
     }
   }
-}
+};
 
 export const deleteFile = async (req, res) => {
   const { fileId } = req.params;
 
-  const authData = await AuthController.checkAccess(req, res);
-  if (!authData.isInvalid) {
-    try {
-      const file = await Files.softDeleteFile(fileId);
-      return res.send({
-        message: "File deleted successfully"
-      });
-    }
-    catch (err) {
-      console.log(err)
-      return res.status(400).json({
-        message: responseMessage.DELETE_ERROR
-      });
-    }
+  try {
+    const file = await Files.softDeleteFile(fileId);
+    return res.send({
+      message: 'File deleted successfully'
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      message: responseMessage.DELETE_ERROR
+    });
   }
-}
+};
--- src/daos/userDao.js
+++ src/daos/userDao.js
@@ -1,54 +1,62 @@
 import User from '../models/user';
 
-export const create = async (data) => {
+export const create = async data => {
   const newUser = await User.forge(data).save();
   return newUser;
-}
+};
 
-export const findByEmail = async (email) => {
+export const findByEmail = async email => {
   const user = await User.forge({ email }).fetch();
   return user;
-}
+};
 
-export const getDetail = async (id) => {
+export const getDetail = async id => {
   const user = await User.forge({ id }).fetch({ withRelated: ['user_avatar'] });
   return user;
-}
+};
 
-export const getUserFiles = async (id) => {
-  const user = await User.forge({ id }).fetch({ withRelated: ['files.user'] });
+export const getUserFiles = async id => {
+  const user = await User.where({ id }).fetch({ withRelated: ['files.user'] });
   const files = user.related('files');
+  let filesList = files.filter(file => !file.attributes.is_deleted);
+  filesList = filesList || [];
+  return filesList;
+};
 
-  return files;
-}
-
-export const updateLogin = async (id) => {
+export const updateLogin = async id => {
   const user = await User.forge({ id }).fetch();
   const currentDate = new Date();
   const change = await user.save({ last_login: currentDate });
 
   return change;
-}
+};
 
 export const update = async (id, data) => {
   const { first_name, last_name, middle_name, gender } = data;
   const user = await getDetail(id);
-  const change = await user.save({ first_name, last_name, middle_name, gender });
+  const change = await user.save({
+    first_name,
+    last_name,
+    middle_name,
+    gender
+  });
 
   return change;
-}
+};
 
 export const updatePassword = async (password, id) => {
   const user = await getDetail(id);
   const change = await user.save({ password });
   return change;
-}
+};
 
 export const fetchAll = async () => {
-  const users = await User.where({ role: 'student' }).fetchAll({ withRelated: ['files', 'user_avatar'] });
+  const users = await User.where({ role: 'student' }).fetchAll({
+    withRelated: ['files', 'user_avatar']
+  });
 
   return users.toJSON();
-}
+};
 
 export const getCount = async () => {
   try {
@@ -58,13 +66,15 @@ export const getCount = async () => {
   } catch (err) {
     throw err;
   }
-}
+};
 
-export const getEducation = async (id) => {
+export const getEducation = async id => {
   try {
-    const education = await User.forge({ id }).fetch({ withRelated: ['educationDetails'] });
+    const education = await User.forge({ id }).fetch({
+      withRelated: ['educationDetails']
+    });
     return education.related('educationDetails');
   } catch (err) {
     throw err;
   }
-}
+};
