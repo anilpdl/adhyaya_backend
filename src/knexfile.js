f7ec790d10786eda37f85c48f7aa4af8fe7bdbec
anlpdl <anil@truemark.com.np>
2020-01-19 17:35:02 +0545

    production knexfile update

--- src/knexfile.js
+++ src/knexfile.js
@@ -31,18 +31,14 @@ module.exports = {
   },
 
   production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
+    client: config.db.client,
+    connection: config.db.connection,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: "knex_migrations"
+      tableName: 'knex_migrations'
     }
   }
 };
