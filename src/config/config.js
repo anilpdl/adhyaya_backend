5a8f5593f442263ff677646ff68a29c1b86ec707
anlpdl <anil@truemark.com.np>
2020-01-19 17:09:40 +0545

    fix api config

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,8 @@
 require('dotenv').config();
 
-// const {
-//   DB_NAME, DB_PASSWORD, DB_USER, DB_HOST, DB_CLIENT
-// } = process.env;
-
-const DB_USER = 'sharma'
-const DB_PASSWORD = ''
-const DB_NAME = 'adhyaya_dev'
-const DB_HOST = 'localhost'
-const DB_CLIENT = 'pg'
+const {
+  DB_NAME, DB_PASSWORD, DB_USER, DB_HOST, DB_CLIENT
+} = process.env;
 
 const config = {
   db: {
