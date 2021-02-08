9a5f730a8f1219eb3f9a2563d97b21f16845cfa5
Anil <anil@truemark.com.np>
2021-02-08 19:30:28 +0545

    update mail data

--- src/constants/mailConstants.js
+++ src/constants/mailConstants.js
@@ -1,7 +1,7 @@
 const mailConfigs = {
   fromEmail: 'support@adhyaya.com.np',
-  templateId: 'd-316ea339deed4619a3c5f0be9c3b703f',
-  passwordResetTemplateId: 'd-fde196cc54e04c60b6d554381299b29c'
+  templateId: 'd-5bd7fd82904941c5b7a661bf82d1f346',
+  passwordResetTemplateId: 'd-e82911f969ee49db81e3c70687b80aa7',
 };
 
 export default mailConfigs;
